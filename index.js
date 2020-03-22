5cc28cdf6bd0352f2f1dbb8dea3f864383495511
serggor4 <sergejgorbacev164@gmail.com>
2020-03-22 19:53:07 +0300

    Creat Server

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log ("Hello World ;3")
+const express = require('express')//подключаем библеотеку express
+const bodyParser = require('body-parser')
+const app = express()//создаёсм экзэмпляр сервера
+
+
+const users = [
+  {
+    name: "Sergey",
+    login: "louhjkiu",
+    pasword: "paqdshkh"
+  },
+  {
+    name: "Sergey2",
+    login: "louhjkiu2",
+    pasword: "paqdshkh2"
+  },
+  {
+    name: "Sergey3",
+    login: "louhjkiu3",
+    pasword: "paqdshkh3"
+  }
+]
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.post('/auch', (req, res) => {
+  //console.log(req.body.login)
+  let user = users.filter(user => user.login == req.body.login && user.pasword == req.body.pasword)[0]
+  res.send(user)
+})
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
